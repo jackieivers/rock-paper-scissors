4d1cd9a9095789f90a2f6451d006f0015b9d7bc8
Jackie <134506520+jackieivers@users.noreply.github.com>
2023-06-18 16:06:18 +1000

    Updated fonts and added visuals for players' selections

--- rps.js
+++ rps.js
@@ -7,9 +7,16 @@ let playerScore = 0;
 let computer = document.querySelector("#computer");
 let compScore = 0;
 
-let whoWon = document.querySelector("#whoWon");
+let playByPlay = document.querySelector("#playByPlay");
+
+let compImg = document.querySelector("#compImg");
+
+let playImg = document.querySelector("#playImg");
+
+let container = document.querySelector("#container");
 
 const resetButton = document.querySelector("#resetButton");
+resetButton.disabled = true;
 const playTo = document.querySelector("#playTo");
 
 let winningScore = 1;
@@ -33,19 +40,21 @@ function computerPlay() {
     }
 }
 
+
 function playRound() {
+    newContainer();
     if (computerSelection === playerSelection) {
-        output.textContent = `It's a tie! You both chose ${computerSelection}`;
+        playByPlay.textContent = `It's a tie! You both chose ${computerSelection}`;
     } else if ((computerSelection === "rock" && playerSelection === "scissors")
         || (computerSelection === "paper" && playerSelection === "rock")
         || (computerSelection === "scissors" && playerSelection === "paper")) {
         compScore += 1;
-        output.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+        playByPlay.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
     } else if ((computerSelection === "rock" && playerSelection === "paper")
         || (computerSelection === "paper" && playerSelection === "scissors")
         || (computerSelection === "scissors" && playerSelection === "rock")) {
         playerScore += 1;
-        output.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+        playByPlay.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
     }
 }
 
@@ -54,14 +63,20 @@ function playGame() {
     player.textContent = playerScore;
     computer.textContent = compScore;
     if (playerScore === winningScore) {
-        whoWon.textContent = "Congrats, you win!";
-        whoWon.classList.add("winner");
+        output.textContent = "Congrats, you win this round!";
+        output.classList.add("winner");
+        player.classList.add("winner");
+        computer.classList.add("loser");
+        resetButton.disabled = false;
         buttons.forEach(elem => {
             elem.disabled = true;
         })
     } else if (compScore === winningScore) {
-        whoWon.textContent = "You lost! This time...";
-        whoWon.classList.add("loser");
+        output.textContent = "You lost! This time...";
+        output.classList.add("loser");
+        computer.classList.add("winner");
+        player.classList.add("loser");
+        resetButton.disabled = false;
         buttons.forEach(elem => {
             elem.disabled = true;
         })
@@ -76,14 +91,42 @@ playTo.addEventListener('change', function () {
 resetButton.addEventListener('click', reset);
 
 function reset() {
+    resetContainer();
     playerScore = 0;
     player.textContent = playerScore;
     compScore = 0;
     computer.textContent = compScore;
     output.textContent = "Choose Your Weapon...";
-    whoWon.textContent = "";
-    whoWon.classList.remove('winner', 'loser');
+    output.classList.remove('winner', 'loser');
+    player.classList.remove("winner", "loser")
+    computer.classList.remove("winner", "loser")
     buttons.forEach(elem => {
         elem.disabled = false;
     });
+    resetButton.disabled = true;
+}
+
+function newContainer() {
+    playByPlay.style.margin = "10px 0px";
+    container.style.backgroundColor = "rgb(243, 208, 208)";
+    container.style.padding = "5px";
+    container.style.width = "300px";
+    container.style.margin = "20px auto";
+    playImg.src = `images/${playerSelection}.png`;
+    playImg.style.width = "75px";
+    playImg.style.margin = "0px 20px"
+    compImg.style.margin = "0px 20px"
+    compImg.src = `images/${computerSelection}.png`;
+    compImg.style.width = "75px";
+
+}
+
+function resetContainer() {
+    playByPlay.textContent = "";
+    playImg.src = "";
+    compImg.src = "";
+    container.style.margin = "0";
+    container.style.width = "0"
+    container.style.backgroundColor = "white";
+    container.style.padding = "0";
 }
